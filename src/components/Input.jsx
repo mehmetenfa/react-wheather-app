269e182f852ab7ee4e5ff027fa0224c5cd173cf1
Mehmet Enfa Vakitsayan <mehmet.enfa@gmail.com>
2022-10-01 16:15:24 +0300

    input finish

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const input = () => {
+const input = ({ getLocation, getInfos }) => {
   return (
     <div className='flex justify-center items-center'>
       <div className='mt-7'>
@@ -8,6 +8,12 @@ const input = () => {
           placeholder="Enter a City"
           type="text"
           className="px-3 py-2 sm:w-[25rem] bg-slate-300 text-black outline-none rounded-lg focus:bg-slate-200 placeholder-text-slate-500"
+          onChange={(e) => getLocation(e.target.value)}
+          onKeyUp={(e) => {
+            if(e.key === "Enter") {
+                getInfos()
+            }
+          }}
         />
       </div>
     </div>
